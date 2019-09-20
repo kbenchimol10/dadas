7144836e6558799077e4c5972510f66541721136
Keven Benchimol <kevenbenchimol@gmail.com>
2019-09-20 14:31:01 +0100

    update the data

--- src/components/GalleryItem.js
+++ src/components/GalleryItem.js
@@ -1,16 +1,18 @@
-import React from 'react';
-import Coaches from '../data/facebook_coaches.json';
-import { BACKEND_URL } from '../constants.js';
-import _ from 'lodash';
+import React from "react";
+import Coaches from "../data/facebook_coaches.json";
+import { BACKEND_URL } from "../constants.js";
+import _ from "lodash";
+import logo from "../images/logo.svg";
 
 class GalleryItem extends React.Component {
   render() {
-    const unique_coach_list = _.uniqBy(Coaches, 'name');
+    const unique_coach_list = _.uniqBy(Coaches, "name");
     return (
       <div className="bg-whitsmoke">
-        <h1 className="text-center font-size pad-b-60">
-          Facebook Coaches Gallery
-        </h1>
+        <div className="title">
+          <img src={logo} alt="logo" />
+          <h1 className="font-size pad-b-60">Facebook Coaches Gallery</h1>
+        </div>
         <div className="grid grid-col-26 grid-center-items">
           {unique_coach_list.map(data => {
             return (
